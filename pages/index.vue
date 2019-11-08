79ee5f4b670b43f0fd5860e32068c0e2b995f52f
StanclM <43635634+StanclM@users.noreply.github.com>
2019-11-08 13:06:55 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -131,7 +131,7 @@
                   <div class="personal-data-item">
                     <div class="personal-data-label">Působiště:</div>
                     <div class="personal-data-value">
-                      1990 - současnost   OS pro Prahu 1 <br>
+                      1990 - současnost   OS pro Prahu 1 (1996 až 2002 místopředsedkyní) <br>
                       
                   1979 – 1988    OS Karlovy Vary <br>
                   1975 - 1979    OS Cheb
